2e62639fc3a618e715a70203e3e2e48554af38a6
Kelaiya <kelaiyarao1@gmail.com>
2017-08-07 11:06:47 -0400

    Life saver

--- myproject/app/components/Maps.js
+++ myproject/app/components/Maps.js
@@ -1,42 +1,51 @@
 import React, { Component } from 'react';
-import { AppRegistry,View, Text, StyleSheet} from 'react-native';
+import { AppRegistry,View, Text, StyleSheet, TouchableHighlight} from 'react-native';
 import MapView from 'react-native-maps';
 import { StackNavigator } from 'react-navigation';
 import suggestions from './Seed';
+import Details from './Details';
 
-
-var locations = [{
-			coordinate:{
-				latitude: 40.7128, 
-				longitude: -74.0059}
-			},
-			{coordinate:{
-				latitude: 40.7150, 
-				longitude: -74.0061}
-			}]
 export default class Maps extends Component {
 	static navigationOptions = {
     title: 'Maps'
   };
 	render(){
-		console.log('hello')
+		console.log('hello');
+		const { navigate } = this.props.navigation;
+		console.log("navigate", navigate)
+		const { params } = this.props.navigation.state;
+
 		return(
 		     <MapView
 		      style={styles.map}
 		      initialRegion={{
-		       	latitude: 40.7128,
-					  longitude: -74.0059,
+		       	latitude: params.latitude,
+					  longitude: params.longitude,
 					  latitudeDelta: 0.0922,
 					  longitudeDelta: 0.0421,
 						}}
 		       >
 		       {
-		       	suggestions.map((suggestion) => (
+		       	suggestions.map((suggestion, i) => (
+
 		       		<MapView.Marker
+		       		key={i}
 					      coordinate={suggestion.coordinate}
 					      title={suggestion.name}
-					      description={suggestion.description}
-			        />
+					      description={suggestion.Address}
+					     >
+					      <MapView.Callout tooltip style={styles.callout}>
+                <View>
+									<TouchableHighlight
+                    onPress={() => navigate('Details', {name: suggestion.name, address: suggestion.Address, phone: suggestion.Phone, description: suggestion.Description})}
+                  >
+                    <View>
+                        <Text>{suggestion.name}</Text>
+                    </View>
+                  </TouchableHighlight>
+                </View>
+              </MapView.Callout>
+             </MapView.Marker>
 			       ))
 		       	}
 		      </MapView>
@@ -45,7 +54,7 @@ export default class Maps extends Component {
 }
 
 const styles = StyleSheet.create({
-    
+
     map: {
         left: 0,
         right: 0,
@@ -56,4 +65,4 @@ const styles = StyleSheet.create({
 
 });
 
-AppRegistry.registerComponent('Maps', () => Maps);
+AppRegistry.registerComponent('maps', () => maps);
